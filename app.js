e97b400c10fad78abaa7176ad9cf880702389790
osipa728 <osipa728@newschool.edu>
2021-12-14 13:58:37 -0500

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
     //DOM load event
     window.addEventListener("DOMContentLoaded", () => {
         const spotlight = document.querySelector('.spotlight');
-        let spotlightSize = 'transparent 50px, rgba(0, 0, 0, 1) 250px)';
+        let spotlightSize = 'transparent 50px, rgba(0, 0, 0, 1) 300px)';
         window.addEventListener('mousemove', e => updateSpotlight(e));
         window.addEventListener('mousedown', e => {
             spotlightSize = 'transparent 130px, rgba(0, 0, 0, 1) 150px)';
             updateSpotlight(e);
         });
         window.addEventListener('mouseup', e => {
-            spotlightSize = 'transparent 50px, rgba(0, 0, 0, 1) 250px)';
+            spotlightSize = 'transparent 50px, rgba(0, 0, 0, 1) 300px)';
             updateSpotlight(e);
         });
         function updateSpotlight(e) {
